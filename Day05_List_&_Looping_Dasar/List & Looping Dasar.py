8399cc91ad61f9a8fb3987382ff6abe9d9c4d025
Muhammad Wafiq Afansa <141570068+Xpurple27@users.noreply.github.com>
2025-05-08 15:42:08 +0700

    Create List & Looping Dasar.py

---
+++ Day05_List_&_Looping_Dasar/List & Looping Dasar.py
@@ -0,0 +1,18 @@
+# Day 05 - List & Looping Dasar
+
+print("=== Program Daftar Kegiatan Harian ===")
+
+# Buat list kosong
+List = []
+
+# Input kegiatan Mu
+n = int(input("Seberapa banyak kegitatan mu ? "))
+
+for i in range(n):
+    kegiatan = input(f"Masukkan kegiatan ke-{i+1}: ")
+    List.append(kegiatan)
+
+# Tampilkan daftar kegiatan
+print("\nDaftar kegiatan harian kamu:")
+for no, kegiatan in enumerate(List, start=1):
+    print(f"{no}. {kegiatan}")
